8dc8392ff81766deeff685210b84342c2ddd034c
VladiMun <mynvladimir@gmail.com>
2021-03-04 21:21:52 +0900

    New Version

--- src/CalculatorVer1.java
+++ src/CalculatorVer1.java
@@ -3,12 +3,42 @@ import java.util.Scanner;
 public class CalculatorVer1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the expression: ");
+        System.out.println("Input the expression: ");
         String input = sc.nextLine();
 
         //System.out.println(input); //check if input is correct
 
         String[] stringar = input.split(" ");
+
+        if (stringar.length != 3) {
+            try {
+                throw new Exception("Use this format: X + X, or 5 + 5");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(2);
+            }
+        } else if (!romanDetectionNumber(stringar[0]) && !isNumeric(stringar[0])) {
+            try {
+                throw new Exception("Use roman or arabic numbers from 1 to 10 only!");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        } else if (!romanDetectionNumber(stringar[2]) && !isNumeric(stringar[2])) {
+            try {
+                throw new Exception("Use roman or arabic numbers from 1 to 10 only");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        } else if (!operatorDetection(stringar[1])) {
+            try {
+                throw new Exception("Use the following operators: +, -, *, /");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(3);
+            }
+        }
 
         /*for (int i = 0; i < stringar.length; i++) { //check if creating string array with 3 elements
             System.out.println(stringar[i]);
@@ -20,22 +50,64 @@ public class CalculatorVer1 {
             System.out.println(romnum1()[i]);
         }*/
 
-        if (romanDetectionNumber(stringar[0])) {
+        if (romanDetectionNumber(stringar[0]) && romanDetectionNumber(stringar[2])) {
             int x3 = Roman.getArabicNumber(stringar[0]);
             int x4 = Roman.getArabicNumber(stringar[2]);
             int result = cal(stringar, x3, x4);
-            System.out.println("Your output" + Roman.getRomanResult(result));
-        } else if (!romanDetectionNumber(stringar[0])) {
+            System.out.println("Your output: " + Roman.getRomanResult(result));
+        }else if (!romanDetectionNumber(stringar[0]) && !romanDetectionNumber(stringar[2])
+        && !isArabicNumberFrom1to10(stringar[0], stringar[2])) {
+            try {
+                throw new Exception("Use numbers from 1 to 10 only");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(5);
+            }
+        } else if (!romanDetectionNumber(stringar[0]) && !romanDetectionNumber(stringar[2])
+                && isArabicNumberFrom1to10(stringar[0], stringar[2])) {
             int x1 = Integer.parseInt(stringar[0]);
             int x2 = Integer.parseInt(stringar[2]);
             System.out.println("Your output: " + cal(stringar, x1, x2));
+        }else {
+            try {
+                throw new Exception("Use only roman or arabic numbers at the same time");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(4);
+            }
         }
-
     }
 
     public static String[] romnum1() {
         String[] romnum1 = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         return romnum1;
+    }
+
+    public static String[] operators() {
+        String[] operators = {"+", "-", "*", "/"};
+        return operators;
+    }
+
+    public static boolean operatorDetection(String st) {
+        boolean result = true;
+        for (int i = 0; i < operators().length; i++) {
+            if (st.contains(operators()[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isArabicNumberFrom1to10(String a, String b){
+        int x1 = Integer.parseInt(a);
+        int x2 = Integer.parseInt(b);
+        boolean result;
+        if(x1 > 0 && x1 < 11 && x2 > 0 && x2 < 11){
+            result = true;
+        } else{
+            result = false;
+        }
+        return result;
     }
 
     public static int sum(int a, int b) {
@@ -80,13 +152,21 @@ public class CalculatorVer1 {
     public static boolean romanDetectionNumber(String st) {
         boolean result = true;
         for (int i = 0; i < romnum1().length; i++) {
-            if (st.contains(romnum1()[i])) {
+            if (st.equals(romnum1()[i])) {
                 return true;
             }
         }
         return false;
     }
 
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
 }
 
@@ -132,7 +212,7 @@ class Roman {
         String result = "0";
         switch (input) {
             case 0:
-                result = "";
+                result = "-";
                 break;
             case 1:
                 result = "I";
@@ -202,7 +282,9 @@ class Roman {
         String number = String.valueOf(a);
         String[] stringar1 = number.split("");
 
-        if (length == 2) {
+        if(a == 0){
+            result = "0";
+        }else if (length == 2) {
             String decimal1 = stringar1[0].concat("0");
             int deci = Integer.parseInt(decimal1);
             int single = Integer.parseInt(stringar1[1]);
